2afd940604b8045eaaf51be2e15d6b2ca1a45884
ysbd1205 <ys.bd.1205@gmail.com>
2018-12-27 02:30:41 +0900

    first commit

---
+++ ibusmozc.sh
@@ -0,0 +1,20 @@
+#clang 
+#libdbus-1-dev 
+#libglib2.0-dev  already
+#libgtk2.0-dev  already
+#subversion  already
+#tegaki-zinnia-japanese  already
+#debhelper  already
+#libibus-1.0-dev 
+#build-essential  already
+#libssl-dev  already
+#libxcb-xfixes0-dev  already
+#python-dev  already
+#gyp 
+#protobuf-compiler 
+#libqt4-dev  already
+#libuim-dev 
+#libzinnia-dev 
+#fcitx-libs-dev 
+#devscripts 
+#ninja-build
