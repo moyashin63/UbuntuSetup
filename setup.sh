84609404d2e48d01cadfb9d760c08c2509140b05
ysbd1205 <ys.bd.1205@gmail.com>
2019-01-10 16:09:41 +0900

    no updates

--- setup.sh
+++ setup.sh
@@ -22,7 +22,7 @@ git clone https://github.com/powerline/fonts.git --depth=1
 cd fonts
 ./install.sh
 cd ..
-rm -r fonts
+sudo rm -r fonts
 sudo apt install -y python-pip
 pip install --upgrade pip
 pip install --user powerline-shell
