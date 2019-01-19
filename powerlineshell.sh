5ee0c865d6680118c5d9fce9d06b3a3e94c59f95
ysbd1205 <ys.bd.1205@gmail.com>
2019-01-19 19:24:00 +0900

    no updates

--- powerlineshell.sh
+++ powerlineshell.sh
@@ -4,6 +4,9 @@ cd fonts
 ./install.sh
 cd ..
 sudo rm -r fonts
+
+gsettings set org.gnome.desktop.interface monospace-font-name 'Uvuntu Mono derivative Powerline 13'
+
 sudo apt install -y python-pip
 pip install --upgrade pip
 pip install --user powerline-shell
