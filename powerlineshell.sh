5ecca27183998593924654638b9219b7acf534d4
ysbd1205 <ys.bd.1205@gmail.com>
2019-01-28 03:22:30 +0900

    no updates

--- powerlineshell.sh
+++ powerlineshell.sh
@@ -4,8 +4,6 @@ git clone https://github.com/powerline/fonts.git --depth=1 && cd fonts
 cd ..
 echo $PASSWORD | sudo -S rm -r fonts
 
-gsettings set org.gnome.desktop.interface monospace-font-name 'Ubuntu Mono derivative Powerline 13'
-
 echo $PASSWORD | sudo -S apt-get install -y python-pip
 pip install --user powerline-shell
 
