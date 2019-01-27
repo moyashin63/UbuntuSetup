5a7cb946c124f754898614bdaf0ac803a2a03441
ysbd1205 <ys.bd.1205@gmail.com>
2019-01-28 03:33:44 +0900

    no updates

--- googlechrome.sh
+++ googlechrome.sh
@@ -1,5 +1,6 @@
-echo $PASSWORD | sudo -S sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
-echo $PASSWORD | sudo -S wget -q -O - https://dl-ssl.google.com/linux/linux_singing_key.pub | sudo apt-key add -
-echo $PASSWORD | sudo -S sed -e '1d' /etc/apt/sources.list.d/google.list
-echo $PASSWORD | sudo -S apt-get update && sudo apt-get install google-chrome-stable
+sudo echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list
+sudo wget -q -O - https://dl-ssl.google.com/linux/linux_singing_key.pub | sudo apt-key add -
+sudo apt-get update && sudo apt-get install google-chrome-stable
+sudo sed -e '1d' /etc/apt/sources.list.d/google.list
+sudo apt-get update
 echo "--------Installed Google Chrome--------"
