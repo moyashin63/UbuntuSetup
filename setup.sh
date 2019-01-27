ea452d9b8ed80160b2ec7ab655c241f96ced1e3b
ysbd1205 <ys.bd.1205@gmail.com>
2019-01-28 03:24:02 +0900

    no updates

--- setup.sh
+++ setup.sh
@@ -47,13 +47,10 @@ git clone https://github.com/powerline/fonts.git --depth=1 && cd fonts
 ./install.sh
 cd ..
 echo $PASSWORD | sudo -S rm -r fonts
-
-gsettings set org.gnome.desktop.interface monospace-font-name 'Uvuntu Mono derivative Powerline 13'
-
 echo $PASSWORD | sudo -S apt-get install -y python-pip
 pip install --user powerline-shell
 
-gsettings set org.gnome.desktop.interface monospace-font-name 'Uvuntu Mono derivative Powerline 13'
+gsettings set org.gnome.desktop.interface monospace-font-name 'Ubuntu Mono derivative Powerline 13'
 echo "--------Installed Powerline Shell--------"
 
 
@@ -64,4 +61,5 @@ cd ~
 git clone https://github.com/ysbd1205/excavation.git
 
 cd ~/UbuntuSetup
+
 . googlechrome.sh
